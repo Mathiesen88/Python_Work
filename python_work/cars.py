cc4674c405471367a2538e4a39993c00e4ce0d37
Martin Mathiesen <martinjuhl@mathies.biz>
2025-01-05 13:56:30 +0100

    First commit

---
+++ python_work/cars.py
@@ -0,0 +1,24 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+print(sorted(cars)) # sorts the list temporrily
+
+cars.reverse() # reverses the order of the list
+print(cars)
+
+len(cars) # returns the number of elements in the list
+print(len(cars))
+
+locations = ['paris','london','new york','tokyo','rome']
+print(locations)
+
+print(sorted(locations)) # sorts the list temporarily
+print(locations)
+
+print(sorted(locations, reverse=True)) # sorts the list temporarily in reverse order
+print(locations)
+
+locations.reverse() # reverses the order of the list
+print(locations)
+locations.reverse() # reverses the order of the list agains
+print(locations)
